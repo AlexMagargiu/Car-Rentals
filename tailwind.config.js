fdd20f340a286bde4a59ea2a1de869fd8da1e4d0
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-10-09 20:57:41 +0300

    Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#FA5F55",
+        accordion: "#343434",
+      },
+    },
     fontFamily: {
       roboto: ["Roboto", "sans-serif"],
     },
