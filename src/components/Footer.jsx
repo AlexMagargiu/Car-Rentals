909be0c84e88bd1475a945c5a21918861f2a785c
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-01 12:25:55 +0200

    Basic mobile styling for Footer components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import { FaPhoneAlt } from "react-icons/fa";
+import { AiFillMail } from "react-icons/ai";
+
+export default function Footer() {
+  return (
+    <section className="max-w-screen-xl mx-auto px-6 flex flex-col items-center gap-16 pb-24">
+      <div className="flex flex-col items-center text-center gap-2">
+        <h4 className="text-2xl">
+          <span className="font-bold">CAR</span> Rental
+        </h4>
+        <p className="max-w-[14rem] opacity-50">
+          We offer a big range of vehicles for all your driving needs. We have
+          the perfect car to meet your needs.
+        </p>
+      </div>
+      <div className="flex flex-col text-lg">
+        <div className="flex items-center gap-4 font-bold">
+          <FaPhoneAlt />
+          0712 345 678
+        </div>
+        <div className="flex items-center gap-4 font-bold">
+          <AiFillMail />
+          carrental@gmail.com
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-2 text-lg">
+        <h4 className="font-bold text-2xl">COMPANY</h4>
+        <p>Careers</p>
+        <p>Mobile</p>
+        <p>Blog</p>
+        <p>How we work</p>
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <h4 className="font-bold text-2xl">WORKING HOURS</h4>
+        <p>Mon - Fri: 9:00AM - 9:00PM</p>
+        <p>Sar: 9:00AM - 7:00PM</p>
+        <p>Sun: Closed</p>
+      </div>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h4 className="font-bold text-2xl">SUBSCRIPTION</h4>
+        <p>Subscribe your Email address for latest news & updates.</p>
+        <input
+          type="text"
+          placeholder="Enter Email Address"
+          className="w-full py-4 px-8 bg-border_faq outline-none"
+        ></input>
+        <button className="flex justify-center gap-2 text-white bg-secondary_text w-full py-4 font-bold">
+          Submit
+        </button>
+      </div>
+    </section>
+  );
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import Banner from "../components/Banner";
 import WhyUs from "../components/WhyUs";
 import Testimonials from "../components/Testimonials";
 import FAQ from "../components/FAQ";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <WhyUs />
       <Testimonials />
       <FAQ />
+      <Footer />
     </section>
   );
 }
