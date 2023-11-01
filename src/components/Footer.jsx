71d1e24efaf372d1acb597ed86a69c783a410024
AlexMagargiu <alexandrumaga2000@gmail.com>
2023-11-01 13:02:24 +0200

    Change some placements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { AiFillMail } from "react-icons/ai";
 
 export default function Footer() {
   return (
-    <section className="max-w-screen-xl mx-auto px-6 grid grid-cols-1 items-center gap-16 pb-24 md:grid-cols-2 xl:flex xl:w-full xl:justify-between">
-      <div className="flex flex-col items-center text-center gap-8">
+    <section className="max-w-screen-xl mx-auto px-6 grid grid-cols-1 gap-16 pb-24 md:grid-cols-2 xl:flex xl:w-full xl:justify-between xl:items-start">
+      <div className="flex flex-col items-center text-center gap-8 md:items-start md:text-start">
         <h4 className="text-2xl">
           <span className="font-bold">CAR</span> Rental
         </h4>
@@ -23,20 +23,20 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="flex flex-col items-center gap-2 text-lg">
+      <div className="flex flex-col items-center gap-2 text-lg md:items-start">
         <h4 className="font-bold text-2xl">COMPANY</h4>
         <p>Careers</p>
         <p>Mobile</p>
         <p>Blog</p>
         <p>How we work</p>
       </div>
-      <div className="flex flex-col items-center gap-2">
+      <div className="flex flex-col items-center gap-2 md:items-start">
         <h4 className="font-bold text-2xl">WORKING HOURS</h4>
         <p>Mon - Fri: 9:00AM - 9:00PM</p>
         <p>Sar: 9:00AM - 7:00PM</p>
         <p>Sun: Closed</p>
       </div>
-      <div className="flex flex-col items-center gap-2 text-center">
+      <div className="flex flex-col items-center gap-2 text-center md:items-start">
         <h4 className="font-bold text-2xl">SUBSCRIPTION</h4>
         <p>Subscribe your Email address for latest news & updates.</p>
         <input
